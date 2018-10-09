50668de8c570c84a2a394ae69cc5c18daf7434bf
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-10-08 22:30:46 -0300

    PaginationHelper#page_count - returns the number of pages

--- lib/pagination_helper.rb
+++ lib/pagination_helper.rb
@@ -1,9 +1,25 @@
 class PaginationHelper
-  def initialize(items:)
+  def initialize(items:, items_per_page:)
     @items = items
+    @items_per_page = items_per_page
+    @pages = build_pages
   end
 
   def item_count
     @items.size
+  end
+
+  def page_count
+    pages.size
+  end
+
+  private
+
+  attr_reader :items, :pages, :items_per_page
+
+  def build_pages
+    pages = []
+    items.each_slice(items_per_page) { |page| pages.push page }
+    pages
   end
 end
--- spec/lib/pagination_helper_spec.rb
+++ spec/lib/pagination_helper_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 require 'pagination_helper'
 
 RSpec.describe PaginationHelper do
-  subject { PaginationHelper.new(items: %w[a b c d e f]) }
+  subject { PaginationHelper.new(items: %w[a b c d e f], items_per_page: 4) }
 
   describe '#item_count' do
     it 'returns the number of items within the entire collection' do
       expect(subject.item_count).to eq 6
+    end
+  end
+
+  describe '#page_count' do
+    it 'returns the number of pages' do
+      expect(subject.page_count).to eq 2
     end
   end
 end
